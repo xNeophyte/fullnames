d30299b0fbe85909f7398532781e21470a5e5340
Edmund Wright <nekurasky@gmail.com>
2018-10-24 16:31:51 -0500

    adding blank environments file for prof to know where to use api key

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const firebaseConfig = {
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: ''
+};
+
+export const environment = {
+  production: false,
+};
